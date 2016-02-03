6726bdb993368a94183c0819bf22d32f1de8055d
jakepic1 <jake@sayhello.com>
2016-02-03 14:11:46 -0800

    Include s3 wagon

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+  :plugins [[s3-wagon-private "1.2.0"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
